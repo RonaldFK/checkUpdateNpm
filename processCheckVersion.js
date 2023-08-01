2040aeaecb39fab9be6837d08af32f7ad7e9b18a
Ronald <ronaldfk.pro@gmail.com>
2023-08-01 10:20:50 +0200

    update files

--- processCheckVersion.js
+++ processCheckVersion.js
@@ -1,9 +1,9 @@
-import * as fs from 'fs';
-import dayjs from 'dayjs';
+import * as fs from "fs";
+import dayjs from "dayjs";
 
-import { processUpdating } from './processUpdating.js';
+import { processUpdating } from "./processUpdating.js";
 const date =
-  '\n' + '\n' + dayjs().format('[Date :] YYYY-MM-DDTHH:mm:ss') + '\n';
+  "\n" + "\n" + dayjs().format("[Date :] YYYY-MM-DDTHH:mm:ss") + "\n";
 // clone initial list to be able to add lastest online version.
 let onlineVersion = [];
 export const processCheckVersion = async (dependencies) => {
@@ -13,10 +13,10 @@ export const processCheckVersion = async (dependencies) => {
   let ibis = 0;
   for (let i = 0; i < dependencies.length; i++) {
     const body = await fetch(
-      `https://registry.npmjs.org/${dependencies[i].nom_module}`,
+      `https://registry.npmjs.org/${dependencies[i].nom_module}`
     );
     const data = await body.json();
-    const latestVersion = data['dist-tags'].latest;
+    const latestVersion = data["dist-tags"].latest;
     const describeModule = data.versions[`${latestVersion}`]?.description;
     onlineVersion[i].onlineVersion = latestVersion;
     onlineVersion[i].descriptionModule = describeModule;
@@ -27,12 +27,12 @@ export const processCheckVersion = async (dependencies) => {
     if (elem.onlineVersion != elem.currentVersion) {
       // console.log('idem', elem.currentVersion);
       fs.appendFile(
-        './checkUpdateNpmLogs/result.log',
+        "./checkUpdateNpmLogs/result.log",
         `Update available for  : ${elem.nom_module} | Current version : ${elem.currentVersion} | Update version : ${elem.onlineVersion}\n`,
         function (err) {
           if (err) throw err;
           // console.log('Updated!');
-        },
+        }
       );
     }
   });
@@ -40,15 +40,15 @@ export const processCheckVersion = async (dependencies) => {
   // logging all dependencies version
   const data = JSON.stringify(onlineVersion);
   await fs.appendFile(
-    './checkUpdateNpmLogs/result.json',
+    "./checkUpdateNpmLogs/result.json",
     date.toString(),
     function (err) {
       if (err) throw err;
       // console.log('Updated!');
-    },
+    }
   );
 
-  await fs.appendFile('./checkUpdateNpmLogs/result.json', data, function (err) {
+  await fs.appendFile("./checkUpdateNpmLogs/result.json", data, function (err) {
     if (err) throw err;
   });
   processUpdating(onlineVersion);
